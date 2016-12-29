6c9d3bd628dc88331c50c323fa4bc72c441f67b2
Dave Jeffrey <mail@davidjeffrey.co.uk>
2016-12-29 00:40:58 +0000

    Bump version. Add changelog. Bind funs and vals.

--- src/alpaca_shell.erl
+++ src/alpaca_shell.erl
@@ -2,7 +2,8 @@
 
 -export([start/0, server/0]).
 
--record(repl_state, {functions = [], 
+-record(repl_state, {bindings = [], 
+                     funs = [],
                      types = []}).
 
 %% Entrypoints
@@ -11,41 +12,56 @@ start() ->
   spawn(fun () -> server() end).
 
 server() ->
+  %% Trap exits
   process_flag(trap_exit, true),
-  % Print welcome banner
-  io:put_chars(" == Alpaca Shell 0.01a == \n\n"
+  %% Print welcome banner
+  io:put_chars(" == Alpaca Shell 0.02a == \n\n"
                " (hint: exit with ctrl-c, run expression by terminating with"
                " ';;' or an empty line)\n\n"),
-  % Enter main server loop
+  %% Enter main server loop
   server_loop(#repl_state{}). 
 
 %% RESULT PRINTING
 
-% Format the result
+%^ Format the result
 output_result(Result) when is_binary(Result) -> 
-  io:format("~s\n", [Result]);
-
-output_result(Result) when is_function(Result, 0) ->
-  output_result(Result());
+  io:format("-- ~s\n", [Result]);
 
 output_result(Result) -> 
-  io:format("~w\n", [Result]).
+  io:format("-- ~s\n", [format_value(Result)]).
  
 %% EXPRESION EXECUTION
 
-% Takes a compile 'expression' module and executes its single main function,
-% displaying the result
+%% Takes a compile 'expression' module and executes its single main function,
+%% displaying the result
 run_expression(Funs, Bin) ->
-  % Load the module
+  %% Load the module
   code:load_binary(alpaca_user_shell, Funs, Bin),
-  % Execute the fake function  
-  % Display the result as best we can
-  % Alpaca can still error at runtime in some cases
-  % so we execute in another process  
+  %% Execute the fake function  
+  %% Display the result as best we can
+  %% Alpaca can still error at runtime in some cases
+  %% so we execute in another process  
   spawn(fun() -> 
     Result = alpaca_user_shell:main({}),  
     output_result(Result) 
   end).
+
+run_bind(Funs, Bin) ->
+  code:load_binary(alpaca_user_shell, Funs, Bin),
+  Pid = spawn_link(fun() -> 
+    try alpaca_user_shell:main({}) of
+      Res -> exit({ok, Res})
+    catch      
+      error:Err -> exit(Err)
+    end
+  end),
+  receive    
+    {'EXIT', Pid, {ok, Res}} -> {ok, Res};
+    {'EXIT', Pid, Other} -> Other
+  after 5000 ->
+    exit(Pid, timeout),
+    {error, "Execution timed out"}
+  end.
 
 %% ERROR PRINTING
 
@@ -65,6 +81,14 @@ print_error({badmatch, {error, {Line, alpaca_parser, [Error, Detail]}}}) ->
     "\x1b[31m -- Syntax Error: ~B: ~s~s\n\x1b[0m", 
     [adjust_line(Line), Error, Detail]);
 
+print_error({cannot_unify, _, _, TypeOne, TypeTwo}) ->
+  io:format(
+    "\x1b[31m -- Type Mismatch: A ~s was expected but a ~s was provided\n\x1b[0m", [TypeOne, TypeTwo]);
+
+print_error({duplicate_definition, Name, _}) ->
+ io:format(
+    "\x1b[31m -- Already defined: ~s\n\x1b[0m", [Name]);
+
 print_error(Other) when is_list(Other) ->
   io:format("\x1b[31m -- Unknown Error: ~s\n\x1b[0m", [Other]);
 print_error(Other) ->
@@ -72,12 +96,13 @@ print_error(Other) ->
  
 %% COMPILING
 compile(Module) ->
-  % This can hang or crash, so run in another process    
+  %% This can hang or crash, so run in another process    
   Pid = spawn_link(fun () -> 
                try alpaca:compile({text, Module}) of
                  Res -> exit({compiled, Res})
                catch 
-                 error:Err -> exit({error, Err})
+                 error:Err -> exit({error, Err});
+                 Anything -> exit(Anything)
                end
              end),
   receive    
@@ -88,16 +113,34 @@ compile(Module) ->
     {error, "Compiler timed out"}
   end.
 
-build_module(State = #repl_state{functions = Funs}) ->
-  "module alpaca_user_shell \n\n"
-  "export main/1 \n\n" ++
-  (string:join(Funs, "")).
+format_value(Record = #{'__struct__' := record}) ->
+  NoStruct = maps:filter(fun(K, _) -> K =/= '__struct__' end, Record),
+  RecordParts = lists:map(fun({K, V}) ->
+                  atom_to_list(K) ++ " = " ++ format_value(V)
+                end, maps:to_list(NoStruct)),
+  "{" ++ string:join(RecordParts, ", ") ++ "}";
+format_value(V) when is_atom(V) ->
+    io_lib:format(":~w", [V]);
+format_value(V) -> io_lib:format("~w", [V]).
+
+build_module(State = #repl_state{bindings = Bindings, funs = Funs}) ->
+  BindingsList = lists:map(fun({Name, Result}) ->
+                   "let " ++ Name ++ " = " ++ format_value(Result) ++ " in "
+                 end, Bindings),
+  BindingsString = string:join(BindingsList, "\n"),
+  FunsList = lists:map(fun(F) ->
+               "let " ++ F ++ " in "
+             end, Funs),
+  FunsString = string:join(FunsList, "\n"),
+  "module user_shell \n\n"
+  "export main/1 \n\n"
+  "main () = " ++ BindingsString ++ FunsList.
   
 handle_expression(Expr, State) ->
-  % Construct a fake module and inject the entered expression
-  % into a fake function main/1 so we can call it from Erlang
-  % Compile the module
-  Module = build_module(State) ++ "main () = \n" ++ Expr,  
+  %% Construct a fake module and inject the entered expression
+  %% into a fake function main/1 so we can call it from Erlang
+  %% Compile the module
+  Module = build_module(State) ++ "\n" ++ Expr,  
   case compile(Module) of
     {ok, Funs, Bin} -> run_expression(Funs, Bin);
     {error, Err} -> print_error(Err);
@@ -105,11 +148,21 @@ handle_expression(Expr, State) ->
   end,
   State.
 
-handle_function_def(Fun, State = #repl_state{functions = Funs}) ->  
-  StateWithNewFun = State#repl_state{functions = [Fun | Funs]},
-  Module = build_module(StateWithNewFun),
+handle_bind(Expr, State = #repl_state{bindings = Bindings, funs = FunBinds}, [_, _, Name, Args]) ->  
+  BindingExpr = "let " ++ Expr ++ " in " ++ Name ++ "\n\n",    
+  Module = build_module(State) ++ BindingExpr,
   case compile(Module) of
-    {ok, _, _} -> StateWithNewFun;
+    {ok, Funs, Bin} -> 
+      case Args of
+        %% Value bind - execute the expression and store the result
+        "" -> case run_bind(Funs, Bin) of
+                {ok, Result} -> 
+                  State#repl_state{bindings = Bindings ++ [{Name, Result}]};
+                Other -> print_error(Other), State
+              end;
+        %% Function bind - we don't need to run it, just store the expression
+        _ -> State#repl_state{funs = FunBinds ++ [Expr]}
+      end;
     {error, Err} -> print_error(Err), State;
     Other -> print_error(Other), State
   end.
@@ -118,11 +171,11 @@ handle_function_def(Fun, State = #repl_state{functions = Funs}) ->
 
 % Run through a list of regular expressions,
 % breaking on the first one that matches
-input_filter(Input, []) -> expression;
+input_filter(Input, []) -> {expression, []};
 input_filter(Input, [{Name, Exp} | Rest]) ->
-  case re:run(Input, Exp) of
+  case re:run(Input, Exp, [{capture, all, list}]) of
     nomatch -> input_filter(Input, Rest);    
-    _ -> Name
+    {match, Captures} -> {Name, Captures}
   end. 
 
 % Try and identify what sort of input the user entered.
@@ -134,12 +187,13 @@ parse_input(Input) ->
     [{empty, "^(\s*)\n$"}, % Empty input
      {expression, "^(\s*)let"}, % Let bind
      {expression, "^(\s*)match"}, % Match expression
-     {function_def, "^(\s*)([a-z][a-zA-Z]*)\s+(.*?)="} % function
+     {bind, "^(\s*)([a-z][a-zA-Z]*)\s+(.*?)="} % bind
     ]).
 
-% Strip ;; if line terminates with it
+% Strip ;; and newline terminators
 strip_terminator(Line) ->
-  re:replace(Line, ";;\n$", "\n\n", [{return, list}]).
+  L = re:replace(Line, ";;\n$", "", [{return, list}]),
+  re:replace(L, "\n$", "", [{return, list}]).
 
 % Termination happens if a line is empty or terminates with ;;
 line_terminates(Line) ->
@@ -163,9 +217,8 @@ server_loop(State) ->
   % Collect input - supporting functions or types currently  
   Input = read_input(" -> "),  
   State_ = case parse_input(Input) of
-    empty -> io:format(" -- Nothing entered\n\n");    
-    expression -> handle_expression(Input, State);
-    function_def -> handle_function_def(Input, State)
+    {empty, _} -> io:format(" -- Nothing entered\n\n");    
+    {expression, _} -> handle_expression(Input, State);
+    {bind, Captures} -> handle_bind(Input, State, Captures)
   end, 
   server_loop(State_).
-            
